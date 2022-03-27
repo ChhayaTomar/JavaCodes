b7d6f0a30727339e6086f73d52654a87fb57aa33
ChhayaTomar <tomarchhaya12@gmail.com>
2022-03-27 14:49:07 +0530

    Commit

---
+++ src/com/company/dataStructures/stack/Next_greater_element.java
@@ -0,0 +1,31 @@
+package com.company.dataStructures.stack;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class Next_greater_element {
+    public static void main(String[] args) {
+        int[] arr={7,3,4,8,1};
+        int[] res = nextGreater(arr);
+        System.out.println(Arrays.toString(res));
+    }
+    public static int[] nextGreater(int[] arr){
+        int n=arr.length;
+        int[] res=new int[n];
+        Stack<Integer> stack=new Stack<>();
+        stack.push(0);
+        int ci=1;
+
+        while(ci<n){
+            while(!stack.isEmpty() && arr[stack.peek()]<arr[ci]){
+                res[stack.pop()]=arr[ci];
+            }
+            stack.push(ci);
+            ci++;
+        }
+        while(!stack.isEmpty()){
+            res[stack.pop()]=-1;
+        }
+        return res;
+    }
+}
