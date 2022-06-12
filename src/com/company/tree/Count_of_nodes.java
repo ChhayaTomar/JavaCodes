7cd07baad94e329a32443f2192166db9c434d870
ChhayaTomar <101592644+ChhayaTomar@users.noreply.github.com>
2022-06-12 22:25:18 +0530

    Commit

--- src/com/company/tree/Count_of_nodes.java
+++ src/com/company/tree/Count_of_nodes.java
@@ -3,7 +3,7 @@ package com.company.tree;
 public class Count_of_nodes {
     public static int countOfNodes(BinaryTree_implementation.TreeNode root){
         if(root==null) return 0;
-        int leftCount=countOfNodes(root.left);
+        int leftCount= countOfNodes(root.left);
         int rightCount=countOfNodes(root.right);
         return (leftCount + rightCount + 1);
     }
